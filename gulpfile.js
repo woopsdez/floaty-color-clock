74459c51e1e7295aad624bec04788261a6014770
Miki Ishijima <woops.mirai@gmail.comm>
2016-04-29 03:58:43 +0900

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var less        = require('gulp-less');
+
+gulp.task('serve', ['less'],function() {
+	browserSync.init({server: "./"});
+	gulp.watch('less/*.less', ['less']);
+	gulp.watch('*.html').on('change',browserSync.reload);
+});
+
+gulp.task('less', function() {
+    return gulp.src('less/*.less')
+    .pipe(less(''))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default',['serve']);
